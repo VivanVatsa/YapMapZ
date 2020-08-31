76d89b6f5a94274cd9d301af8079f46df8b2610b
Vivan <vivanvatsa@gmail.com>
2020-08-31 23:10:39 +0530

    add for coordinates in PY

--- Map1.py
+++ Map1.py
@@ -3,8 +3,12 @@ import folium
 map = folium.Map([38.58, -99.09], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
-map.add_child(folium.Marker(location=[
-              38.2, -99.1], popup="heya im a marker", icon=folium.Icon(color='green')))
+
+for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+    map.add_child(folium.Marker(location=coordinates,
+                                popup="heya im a marker", icon=folium.Icon(color='green')))
+
+# map.add_child(folium.Marker(location=[37.2, -97.1], popup="heya im a marker 2", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
