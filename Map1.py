51e85af645f018b58ff92ecf61bfa2edc8004e37
Vivan <vivanvatsa@gmail.com>
2020-08-31 22:52:52 +0530

    adding feature group

--- Map1.py
+++ Map1.py
@@ -1,6 +1,11 @@
 import folium
 
 map = folium.Map([38.58, -99.09], zoom_start=6)
+
+fg = folium.FeatureGroup(name="My Map")
 map.add_child(folium.Marker(location=[
               38.2, -99.1], popup="heya im a marker", icon=folium.Icon(color='green')))
+
+map.add_child(fg)
+
 map.save("Map1.html")
