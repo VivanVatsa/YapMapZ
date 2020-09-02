c0afc9614685481f17469a95633e75ebb5be7950
Vivan <vivanvatsa@gmail.com>
2020-09-02 22:48:11 +0530

    Update Map1.py

--- Map1.py
+++ Map1.py
@@ -8,8 +8,6 @@ for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
     map.add_child(folium.Marker(location=coordinates,
                                 popup="heya im a marker", icon=folium.Icon(color='green')))
 
-# map.add_child(folium.Marker(location=[37.2, -97.1], popup="heya im a marker 2", icon=folium.Icon(color='green')))
-
 map.add_child(fg)
 
 map.save("Map1.html")
