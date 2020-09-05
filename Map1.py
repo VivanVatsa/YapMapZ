2d24dd3aa3cf9ef874ec1ae820a2232319c8e4a9
Vivan <vivanvatsa@gmail.com>
2020-09-05 21:30:20 +0530

    Update Map1.py

--- Map1.py
+++ Map1.py
@@ -1,12 +1,24 @@
 import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes_USA.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+
 
 map = folium.Map([38.58, -99.09], zoom_start=6)
 
 fg = folium.FeatureGroup(name="My Map")
 
-for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
-    map.add_child(folium.Marker(location=coordinates,
-                                popup="heya im a marker", icon=folium.Icon(color='green')))
+# used a for loop to iterative the data of the csv file into the python file to mark it on the web map and then display it in the form of marker.
+    
+for lt, ln in zip(lat, lon):
+    fg.add_child(folium.Marker(
+        location=[lt, ln], popup="halo im the marker", icon=folium.Icon(color='green')))
+
+# for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
+#     map.add_child(folium.Marker(location=coordinates,
+#                                 popup="heya im a marker", icon=folium.Icon(color='green')))
 
 map.add_child(fg)
 
