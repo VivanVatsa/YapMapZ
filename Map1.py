d9a1953402a46573075d17034bc1b559ed828c29
Vivan <vivanvatsa@gmail.com>
2020-09-06 16:21:38 +0530

    maps edits

--- Map1.py
+++ Map1.py
@@ -13,9 +13,11 @@ fg = folium.FeatureGroup(name="My Map")
 
 # used a for loop to iterative the data of the csv file into the python file to mark it on the web map and then display it in the form of marker.
 
+
+# i can concatenate the elevation popup with metres and the elevation value
 for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.Marker(
-        location=[lt, ln], popup=str(el), icon=folium.Icon(color='green')))
+        location=[lt, ln], popup=str(el) + " m", icon=folium.Icon(color='green')))
 
 # for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
 #     map.add_child(folium.Marker(location=coordinates,
