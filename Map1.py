4da0666b016ef2be57e9d579d55b27e234dcd57b
Vivan <vivanvatsa@gmail.com>
2020-09-08 12:41:30 +0530

    Update Map1.py

--- Map1.py
+++ Map1.py
@@ -33,7 +33,9 @@ for lt, ln, el in zip(lat, lon, elev):
         el) + " m", fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
 # will use lambda function in the style_function parameter
-fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=))
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+                            style_function=lambda x: {'fillC
+                                                      olor': 'yellow'}))
 
 # for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
 #     map.add_child(folium.Marker(location=coordinates,
