111a7d7f584c7522d34d2d366dd40ca2deaf7f5e
Vivan <vivanvatsa@gmail.com>
2020-09-08 12:35:00 +0530

    Update Map1.py

--- Map1.py
+++ Map1.py
@@ -32,6 +32,9 @@ for lt, ln, el in zip(lat, lon, elev):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, popup=str(
         el) + " m", fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
+# will use lambda function in the style_function parameter
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(), style_function=))
+
 # for coordinates in [[38.2, -99.1], [39.2, -97.1]]:
 #     map.add_child(folium.Marker(location=coordinates,
 #                                 popup="heya im a marker", icon=folium.Icon(color='green')))
